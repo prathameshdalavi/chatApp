4f33e703bc29e6039e13265bfc4c45a3907f0a9d
Prathamesh Dalavi <prathameshdalavi04@gmail.com>
2025-02-26 10:13:09 +0530

    updated chatBox

--- chatApp-frontend/src/pages/dashboard.tsx
+++ chatApp-frontend/src/pages/dashboard.tsx
@@ -1,24 +1,32 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Button } from "../components/button";
 import { Input } from "../components/input";
 import { useWebSocket } from "../components/webSocketServer";
 
 export function Dashboard() {
     const InputRef = useRef<HTMLInputElement>(null);
+    const ChatContainerRef = useRef<HTMLDivElement>(null);
     const {messages,sendMessage}=useWebSocket()
     const handleSendMessage = () => {
         const message = InputRef.current?.value || "";
         sendMessage({ message});
     };
     console.log(messages)
+    useEffect(() => {
+        if (ChatContainerRef.current) {
+            ChatContainerRef.current.scrollTop = ChatContainerRef.current.scrollHeight;
+        }
+    }, [messages]);
     return (
-        <div className="flex flex-col items-center justify-center w-full h-screen bg-black">
-            <div className="w-1/2 h-full my-12 rounded-lg  flex flex-col bg-gray-400">
-            <div className="w-full h-full  border-2 p-2 rounded-md flex flex-col gap-2">
+        <div className="flex flex-col items-center justify-center w-full  h-screen bg-slate-900">
+            <div className="text-4xl font-bold text-slate-300 mb-2 ">Live Chat</div>
+            <div className="text-md font-semibold text-slate-300 mb-4 ">Chat with friends in real time!</div>
+            <div className="w-2/5 h-4/5 rounded-2xl border-1  flex flex-col p-2  bg-slate-800">
+            <div ref={ChatContainerRef} className="w-full h-full scroll-smooth  border-2 overflow-y-auto p-2 [&::-webkit-scrollbar]:hidden scrollbar-hide rounded-md flex flex-col gap-2">
             {messages.map((message, index) => (
                     <span
                     key={index}
-                    className={`border-2 rounded-xl flex items-center h-10 shadow-md p-2 ${
+                    className={`border-2 rounded-xl max-w-64 px-4 text-sm py-2 items-center shadow-md text-justify  ${
                         message.isOwnMessage
                             ? "self-end bg-blue-200 text-right border-blue-200"     
                             : "self-start bg-slate-200 text-left"
--- chatApp-frontend/src/pages/roomCreation.tsx
+++ chatApp-frontend/src/pages/roomCreation.tsx
@@ -19,16 +19,16 @@ export function RoomCreation() {
     }
     return (
         <div className="w-full h-screen items-center justify-center flex flex-col bg-gray-800 " >
-            <div className="text-4xl font-bold p-2 text-slate-300 text-white">Welcome to the Chat App </div>
-            <div className="text-md  font-semibold pb-2 text-slate-300 text-white"  >Create a room to start chatting with friends. </div>
-            <div className="w-1/3 h-48 p-2 rounded-2xl  mt-4    flex flex-col gap-3  bg-gray-600">
+            <div className="text-4xl font-bold p-2 text-slate-300 ">Welcome to the Chat App </div>
+            <div className="text-md  font-semibold pb-2 text-slate-300 "  >Create a room to start chatting with friends. </div>
+            <div className="w-96 h-48 p-2 rounded-2xl  mt-4    flex flex-col gap-3  bg-gray-600">
                 <div className="text-3xl font-bold text-slate-300 flex items-center justify-center">
                     Create Room
                 </div>
                 <div className="flex  mt-2">
                     <Input reference={inputRef} placeholder="Enter Room Id" />
                 </div>
-                <div className="flex items-center  justify-center">
+                <div className="flex text-slate-300 items-center  justify-center">
                 <Button  text="Join"  onClick={join} />
                 </div>
             </div>
